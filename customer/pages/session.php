a9ba0b476956a55f04e7b0d40b9e64b77b7719ee
Mohammed Yassin <yssnmed@gmail.com>
2024-02-15 15:25:39 +0100

    customer error fixed

---
+++ customer/pages/session.php
@@ -0,0 +1,13 @@
+<?php
+//Start session
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+//Check whether the session variable SESS_MEMBER_ID is present or not
+if (!isset($_SESSION['user_id']) || (trim($_SESSION['user_id']) == '')) {
+    header("location: index.php");
+    exit();
+}
+$session_id=$_SESSION['user_id'];
+
+?>
